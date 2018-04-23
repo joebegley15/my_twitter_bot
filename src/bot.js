a23008f51598040e3393012e8e0ca602d18c40b3
Joe Begley <joebegley@Joes-MacBook-Air.local>
2018-04-23 17:24:30 -0400

    Tweets from an array of tweets

--- src/bot.js
+++ src/bot.js
@@ -3,15 +3,23 @@ const config = require('./config')
 
 const bot = new Twit(config)
 
-bot.get('followers/list', {
-  screen_name: 'kanyewest',
-  count: 500
-}, (err, data, response) => {
-  if (err) {
-    console.log(err)
-  } else {
-    data.users.forEach(user => {
-    	console.log(user.screen_name);
-    })
-  }
-})
+var willTweet = ['Once I finish debuggin, I will stop, I promise', 'Thank you all for tolerating this.', 'Till next time - Joe\'s Twitter Bot'];
+var i = 0;
+
+var fireMeUp = function(){
+	if (i >= willTweet.length) {
+		clearInterval(timerId);
+	}
+	bot.post('statuses/update', {
+	  status: willTweet[i]
+	}, (err, data, response) => {
+	  if (err) {
+	    console.log(err)
+	  } else {
+	    console.log(`${data.text} tweeted!`)
+	  }
+	})
+	i++;
+}
+
+let timerId = setInterval(() => fireMeUp(), 10000);
